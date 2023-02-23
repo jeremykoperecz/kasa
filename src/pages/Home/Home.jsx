d0edef37745e095f7765809217096a22c237ab16
jeremykoperecz <kopereczjeremy@gmail.com>
2023-02-23 15:45:42 +0100

    finalisation projet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function Home() {
-	const [data, setData] = useState([]);
+	const [appart, setAppart] = useState([]);
 
 	useEffect(() => {
-		axios.get("/logements.json").then((res) => setData(res.data)); //requète AXIOS ici également pour prochaine utilisation API
+		axios.get("/logements.json").then((res) => setAppart(res.data)); //requète AXIOS pour prochaine utilisation API
 	}, []);
-		
+	console.log(appart);
 	
 
 		return (
 			<>
 				<Banner />
 				<div className="cards-container">
-					{data.map((appart, id) => (
+					{appart.map((appart, id) => (
 						<div className="card_logement" key={id}>
 							<Link className="link_card_logement" to={`/logement/${appart.id}`}>
 								<Card cover={appart.cover} title={appart.title} />
